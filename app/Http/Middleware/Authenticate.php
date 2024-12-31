ce89f143f81b5079f7eb474800e1a3cec718412f
abdalhababsah <abdalhababseh@outlook.com>
2024-12-31 10:18:10 +0300

    redirect to correct login page

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -35,6 +35,6 @@ class Authenticate extends BaseAuthenticate
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : route('admin.login');
     }
 }
