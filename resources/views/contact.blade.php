219a9a9e6ede329a6b057cd9b63794eaf6983541
ReemAbdallh <reem@phoenixjo.tech>
2025-01-20 14:37:08 +0300

    edit alnadwa story in home

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -68,9 +68,8 @@
         <div class="container">
             <div class="row mg-pt">
                 <div class="col-lg-12 p-0">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48384.367867189205!2d-74.01058227968896!3d40.71751035716885!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1609671967457!5m2!1sen!2sbd"
-                        width="1920" height="520" style="border:0;" allowfullscreen="" aria-hidden="false"
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3385.091684705637!2d35.85215850000001!3d31.95840930000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ca11283d2dcd3%3A0x898ebd989f56cd89!2sAl%20Nadwa%20Architect%20%26%20Engineering%20Office!5e0!3m2!1sen!2sus!4v1737371996134!5m2!1sen!2sus" 
+                        width="1920" height="520" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" aria-hidden="false"
                         tabindex="0"></iframe>
                 </div>
             </div>
