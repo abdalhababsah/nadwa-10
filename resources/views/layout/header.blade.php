f2823cb335ff8e82fa9e38ed7d568b63f0e85602
ReemHasanA <reem.abdalla98@gmail.com>
2025-02-04 11:51:51 +0300

    add fixed social link to users page

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -31,6 +31,13 @@
         </div>
     </div>
 </div>
+<!-- Fixed Social Aside -->
+<div class="right-mid-fixed">
+    <ul class="social-box">
+        <li class="d-block"><a href="https://www.instagram.com/alnadwa.architecture/"> <i class="fab fa-instagram"></i></a></li>
+        <li class="d-block"><a href="https://www.facebook.com/Al.NadwaArchitects/"> <i class="fab fa-facebook-f"></i></a></li>
+    </ul>
+</div>
 <!--  Mobile Menu Area -->
 <div class="mobile-menu-area sticky d-sm-block d-md-block d-lg-none ">
     <div class="mobile-menu">
@@ -85,19 +92,19 @@
                         <div class="contact-info">
                             <h2>Contact Info</h2>
                             <ul class="list-style-one">
-                                <li><i class="bi bi-envelope"></i> Amman, Jordan</li>
+                                <li><i class="bi bi-envelope"></i>Amman, Jordan</li>
                                 <li><i class="bi bi-envelope"></i>(+962) 797550065</li>
                                 <li><i class="bi bi-envelope"></i>archnadia@hotmail.com</li>
-                                <li><i class="bi bi-envelope"></i>Saturday-: 08.30 to 18.00 Friday & : Closed</li>
+                                <li><i class="bi bi-envelope"></i>Saturday-Thursday: 08.30 to 16.00 Friday : Closed</li>
                             </ul>
                         </div>
                         <!-- Social Box -->
                         <ul class="social-box">
                             <li class="facebook"><a href="https://www.facebook.com/Al.NadwaArchitects/" class="fab fa-facebook-f"></a></li>
-                            <li class="twitter"><a href="https://www.instagram.com/alnadwa.architecture/" class="fab fa-instagram"></a></li>
-                            <!-- <li class="linkedin"><a href="#" class="fab fa-twitter"></a></li>
-                            <li class="instagram"><a href="#" class="fab fa-pinterest-p"></a></li>
-                            <li class="youtube"><a href="#" class="fab fa-linkedin-in"></a></li> -->
+                            <li class="instagram"><a href="https://www.instagram.com/alnadwa.architecture/" class="fab fa-instagram"></a></li>
+                            <!-- <li class="twitter"><a href="#" class="fab fa-twitter"></a></li>
+                            <li class=""><a href="#" class="fab fa-pinterest-p"></a></li>
+                            <li class="linkedin"><a href="#" class="fab fa-linkedin-in"></a></li> -->
                         </ul>
                     </div>
                 </div>		
